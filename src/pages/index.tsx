b7748596841a773ef81ad1f2a17557e0259d0cd7
DiogoAMV <diogo.amv19@gmail.com>
2022-01-07 22:47:48 -0300

    created global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        <Head>
+          <title>Início | ig.news</title>
+          <meta
+            name="description"
+            content="Blog sobre atualizações do ReactJS"
+          />
+        </Head>
+        Hello World!
+      </h1>
+    </>
+  )
+}
