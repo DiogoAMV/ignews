cca33c0c4b3ab7793897ba382af772f878cd1f32
DiogoAMV <diogo.amv19@gmail.com>
2022-01-13 09:55:41 -0300

    :sparkles: added purchase feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,59 @@
+import { GetStaticProps } from 'next'
+import styles from './home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <h1>
         <Head>
-          <title>Início | ig.news</title>
+          <title>Home | ig.news</title>
           <meta
             name="description"
             content="Blog sobre atualizações do ReactJS"
           />
         </Head>
-        Hello World!
+        
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+            </p>
+          <SubscribeButton priceId={product.priceId} />
+          </section>
+          <img src="/images/avatar.svg" alt="Girl coding" />
+        </main>
       </h1>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KG9ifBw9cBpl9BLLpT1GtFn')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+  
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
